d4b20a161b5dbc9d45d512b200c2140514921864
Matt-pioneerly <matt@pioneerly.com>
2026-06-05 23:13:01 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -30,7 +30,8 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site"
+      output: "_site",
+      includes: "_includes"
     },
     markdownTemplateEngine: false,
     htmlTemplateEngine: "njk"
