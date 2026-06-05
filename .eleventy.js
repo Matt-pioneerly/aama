5013f754daf18787e33544d0fbf6b299668627e8
Matt-pioneerly <matt@pioneerly.com>
2026-06-05 23:03:57 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,13 @@
 module.exports = function(eleventyConfig) {
 
-  // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/css");
 
-  // Blog post collection, newest first
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
   });
 
-  // Featured post (first post with featured: true, or just the newest)
   eleventyConfig.addCollection("featuredPost", function(collectionApi) {
     const posts = collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
     return posts.filter(p => p.data.featured).length > 0
@@ -18,7 +15,6 @@ module.exports = function(eleventyConfig) {
       : [posts[0]];
   });
 
-  // Date filter: "April 2026"
   eleventyConfig.addFilter("postDate", function(date) {
     return new Date(date).toLocaleDateString("en-US", {
       month: "long",
@@ -27,7 +23,6 @@ module.exports = function(eleventyConfig) {
     });
   });
 
-  // Slug filter
   eleventyConfig.addFilter("slugify", function(str) {
     return str.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
   });
@@ -35,9 +30,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_includes/layouts"
+      output: "_site"
     },
     markdownTemplateEngine: false,
     htmlTemplateEngine: "njk"
